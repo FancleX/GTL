ea07eb1fbd4c2a69848b2a2965db89ffd08a13e7
chen <fancle666@gmail.com>
2022-04-07 15:45:08 -0400

    initial commit

---
+++ backend/gtl/src/main/java/com/dev/gtl/GtlApplication.java
@@ -0,0 +1,13 @@
+package com.dev.gtl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GtlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GtlApplication.class, args);
+	}
+
+}
---
+++ backend/gtl/src/test/java/com/dev/gtl/GtlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.gtl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GtlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
