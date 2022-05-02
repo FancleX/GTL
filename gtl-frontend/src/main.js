1fe79063be1b95635f1beab542d03c71e999c66e
chen <fancle666@gmail.com>
2022-05-01 20:41:33 -0400

    Update main.js

--- gtl-frontend/src/main.js
+++ gtl-frontend/src/main.js
@@ -2,13 +2,9 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import '../src/config/axiosConfig'
-// import './Listener'
 
 
 createApp(App).use(router).mount('#app')
 
-window.onbeforeunload = function() {
-    localStorage.clear();
-};
 
 
